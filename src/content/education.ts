2ff81b47e7d7d579814fabcfb19d643a527c4017
adilmrf <adilmrf02@gmail.com>
2026-02-10 22:59:10 +0400

    slight changes - spelling mistakes

--- src/content/education.ts
+++ src/content/education.ts
@@ -8,7 +8,7 @@ export const EDUCATION: EducationItem[] = [
     logo: "/media/logos/uaeu.png",
     startDate: "08.2020",
     endDate: "12.2024",
-    grade: "GPA: 3.95/4.0",
+    grade: "GPA: 3.95 /4.0",
   },
   {
     id: "edu-2",
@@ -17,7 +17,7 @@ export const EDUCATION: EducationItem[] = [
     logo: "/media/logos/nyuad.png",
     startDate: "08.2022",
     endDate: "05.2023",
-    grade: "GPA 3.93/4.0",
+    grade: "GPA: 3.93 /4.0",
   },
   {
     id: "edu-3",
